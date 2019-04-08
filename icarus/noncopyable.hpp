153f4cdefd49a20d3bc63278d0ffcd32a371b4b7
cyyzero <cyyzero16@qq.com>
2019-04-08 23:13:29 +0800

    Update `struct noncopyable`

---
+++ icarus/noncopyable.hpp
@@ -0,0 +1,20 @@
+#ifndef ICARUS_NONCOPYABLE_HPP
+#define ICARUS_NONCOPYABLE_HPP
+
+namespace icarus
+{
+// base class, disallow copying
+class noncopyable
+{
+  protected:
+    constexpr noncopyable() = default;
+    ~noncopyable() = default;
+
+  private:
+    noncopyable(const noncopyable &) = delete;
+    noncopyable &operator=(const noncopyable &) = delete;
+};
+
+} // namespace icarus
+
+#endif // ICARUS_NONCOPYABLE_HPP
