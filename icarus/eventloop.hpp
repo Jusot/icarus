ff0a9757534805510a1f06f6de73afbc1b3f886d
mu001999 <mu001999@outlook.com>
2019-04-09 19:20:34 +0800

    add comments

--- icarus/eventloop.hpp
+++ icarus/eventloop.hpp
@@ -6,19 +6,26 @@ namespace icarus
 class EventLoop : noncopyable
 {
   public:
+	// default contructor
     EventLoop();
 
+	// default deconstructor
     ~EventLoop();
 
+	// main loop
     void loop();
 
+	// assert current thread is in loop thread or not
     void assert_in_loop_thread();
 
+	// checks current thread is in loop thread or not
     bool is_in_loop_thread() const;
 
+	// returns EventLoop object's address of current thread
     static EventLoop *get_event_loop_of_current_thread();
 
   private:
+	// abort when not in loop thread
     void abort_not_in_loop_thread();
 
     bool looping_;
