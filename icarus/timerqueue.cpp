01f586797dbaf9dc09dfc43d3df97cc502d4087f
mu001999 <mu001999@outlook.com>
2019-04-14 15:51:01 +0800

    init timerqueue

---
+++ icarus/timerqueue.cpp
@@ -0,0 +1,1 @@
+#include "timerqueue.hpp"
---
+++ icarus/timerqueue.hpp
@@ -0,0 +1,13 @@
+#ifndef ICARUS_TIMERQUEUE_HPP
+#define ICARUS_TIMERQUEUE_HPP
+
+namespace icarus
+{
+class TimerQueue
+{
+  public:
+  private:
+};
+} // namespace icarus
+
+#endif // ICARUS_TIMERQUEUE_HPP
