dc36c3db430de13394fac273be919c3e97ab98f5
cyyzero <cyyzero16@qq.com>
2019-04-22 23:58:23 +0800

    Update `class Poller`

--- icarus/poller.cpp
+++ icarus/poller.cpp
@@ -20,7 +20,7 @@ Poller::~Poller()
 
 std::chrono::system_clock::time_point Poller::poll(int timeout_ms, ChannelList *active_channels)
 {
-    auto num_events = ::poll(&pollfds_[0], pollfds_.size(), timeout_ms);
+    int num_events = ::poll(pollfds_.data(), pollfds_.size(), timeout_ms);
     auto now = std::chrono::system_clock::now();
 
     if (num_events > 0)
