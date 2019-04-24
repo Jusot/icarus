b50c2e25166b5fa4417e375378d2f7bf1283ab65
mu001999 <mu001999@outlook.com>
2019-04-24 22:51:00 +0800

    init eventloopthread class

---
+++ icarus/eventloopthread.cpp
@@ -0,0 +1,58 @@
+#include <cassert>
+
+#include "eventloop.hpp"
+#include "eventloopthread.hpp"
+
+using namespace icarus;
+
+EventLoopThread::EventLoopThread(const ThreadInitCallback& cb, const std::string& name)
+  : loop_(nullptr), 
+    exiting_(false), 
+    callback_(cb)
+{
+    // ...
+}
+
+EventLoopThread::~EventLoopThread()
+{
+    exiting_ = false;
+    if (loop_)
+    {
+        loop_->quit();
+        thread_.join();
+    }
+}
+
+EventLoop *EventLoopThread::start_loop()
+{
+    assert(!thread_.joinable());
+    thread_ = std::thread(this->thread_func);
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while (!loop_)
+        {
+            cond_.wait(lock, [&] { return loop_; });
+        }
+    }
+    return loop_;
+}
+
+void EventLoopThread::thread_func()
+{
+    EventLoop loop;
+
+    if (callback_)
+    {
+        callback_(&loop);
+    }
+
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        loop_ = &loop;
+        cond_.notify_all();
+    }
+
+    loop.loop();
+    std::lock_guard<std::mutex> lock(mutex_);
+    loop_ = nullptr;
+}
---
+++ icarus/eventloopthread.hpp
@@ -0,0 +1,39 @@
+#ifndef ICARUS_EVENTLOOPTHREAD_HPP
+#define ICARUS_EVENTLOOPTHREAD_HPP
+
+#include <mutex>
+#include <string>
+#include <thread>
+#include <functional>
+#include <condition_variable>
+
+#include "noncopyable.hpp"
+
+namespace icarus
+{
+class EventLoop;
+
+class EventLoopThread : noncopyable
+{
+  public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    EventLoopThread(const ThreadInitCallback &cb = ThreadInitCallback(), const std::string &name = std::string());
+    ~EventLoopThread();
+    
+    EventLoop *start_loop();
+  
+  private:
+    void thread_func();
+
+    EventLoop *loop_;
+    bool exiting_;
+    std::thread thread_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    ThreadInitCallback callback_;
+};
+
+} // namespace icarus
+
+#endif // ICARUS_EVENTLOOPTHREAD_HPP
