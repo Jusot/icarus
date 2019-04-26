78b7ea1b8ecca2f981630c2f53506b4209af9a2a
cyyzero <cyyzero16@qq.com>
2019-04-27 01:27:36 +0800

    Add endian conversion functions

--- icarus/socketsfunc.cpp
+++ icarus/socketsfunc.cpp
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <errno.h>
+#include <endian.h>
 
 #include "socketsfunc.hpp"
 
@@ -78,6 +79,36 @@ void close(int sockfd)
     {
         abort();
     }
+}
+
+uint64_t hostToNetwork64(uint64_t host64)
+{
+    return htobe64(host64);
+}
+
+uint32_t hostToNetwork32(uint32_t host32)
+{
+    return htobe32(host32);
+}
+
+uint16_t hostToNetwork16(uint16_t host16)
+{
+    return htobe16(host16);
+}
+
+uint64_t networkToHost64(uint64_t net64)
+{
+    return be64toh(net64);
+}
+
+uint32_t networkToHost32(uint32_t net32)
+{
+    return be32toh(net32);
+}
+
+uint16_t networkToHost16(uint16_t net16)
+{
+    return be16toh(net16);
 }
 
 } // namespace sockets
--- icarus/socketsfunc.hpp
+++ icarus/socketsfunc.hpp
@@ -1,6 +1,8 @@
 #ifndef ICARUS_SOCKHELPER_HPP
 #define ICARUS_SOCKHELPER_HPP
 
+#include <cstdint>
+
 namespace icarus
 {
 
@@ -12,6 +14,13 @@ void listenOrDie(int sockfd);
 int accept(int sockfd, struct sockaddr_in* addr);
 void close(int sockfd);
 void shutdownWrite(int sockfd);
+
+uint64_t hostToNetwork64(uint64_t hosst64);
+uint32_t hostToNetwork32(uint32_t host32);
+uint16_t hostToNetwork16(uint16_t host16);
+uint64_t networkToHost64(uint64_t net64);
+uint32_t networkToHost32(uint32_t net32);
+uint16_t networkToHost16(uint16_t net16);
 
 } // namespace sockets
 } // namespace icarus
