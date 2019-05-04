4b94d08028736744174e6bcf3ddf5b39ee4380cc
cyyzero <cyyzero16@qq.com>
2019-05-04 15:32:54 +0800

    add `class TcpServer`

---
+++ icarus/tcpserver.cpp
@@ -0,0 +1,111 @@
+#include <memory>
+#include <cstdio>
+#include <cassert>
+
+#include "tcpserver.hpp"
+#include "eventloop.hpp"
+#include "acceptor.hpp"
+#include "inetaddress.hpp"
+#include "socketsfunc.hpp"
+#include "tcpconnection.hpp"
+#include "eventloopthreadpool.hpp"
+
+namespace icarus
+{
+
+TcpServer::TcpServer(EventLoop* loop, const InetAddress& listen_addr, const std::string& name)
+  : loop_(loop),
+    host_port_(listen_addr.to_ip_port()),
+    name_(name),
+    acceptor_(new Acceptor(loop, listen_addr)),
+    thread_pool_(new EventLoopThreadPool(loop)),
+    connection_callback_(default_connection_callback),
+    message_callback_(default_message_callback),
+    started_(false),
+    next_conn_id_(1)
+{
+    acceptor_->set_new_connection_callback([this] (int sockfd, const InetAddress& inet_addr) {
+        this->new_connection(sockfd, inet_addr);
+    });
+}
+
+TcpServer::~TcpServer()
+{
+    loop_->assert_in_loop_thread();
+    for (auto& pair: connections_)
+    {
+        pair.second->get_loop()->run_in_loop([conn=pair.second] () {
+            conn->connect_destroyed();
+        });
+    }
+}
+
+void TcpServer::set_thread_num(int num_threads)
+{
+    assert(num_threads >= 0);
+    thread_pool_->set_thread_num(num_threads);
+}
+
+void TcpServer::start()
+{
+    if (!started_)
+    {
+        started_ = true;
+        thread_pool_->start();
+    }
+
+    if (!acceptor_->listenning())
+    {
+        loop_->run_in_loop([ptr = acceptor_.get()] () {
+            ptr->listen();
+        });
+    }
+}
+
+void TcpServer::set_connection_callback(ConnectionCallback cb)
+{
+    connection_callback_ = std::move(cb);
+}
+
+void TcpServer::set_message_callback(MessageCallback cb)
+{
+    message_callback_ = std::move(cb);
+}
+
+void TcpServer::new_connection(int sockfd, const InetAddress &peer_addr)
+{
+    loop_->assert_in_loop_thread();
+    EventLoop* io_loop = thread_pool_->get_next_loop();
+    char buf[32];
+    snprintf(buf, sizeof(buf), ":%s#%d", host_port_.c_str(), next_conn_id_);
+    ++next_conn_id_;
+    std::string conn_name = name_ + buf;
+    InetAddress local_addr(sockets::get_local_addr(sockfd));
+    auto conn = std::make_shared<TcpConnection>(io_loop, conn_name, sockfd, local_addr, peer_addr);
+    connections_[conn_name] = conn;
+    conn->set_connection_callback(connection_callback_);
+    conn->set_message_callback(message_callback_);
+    conn->set_write_complete_callback(write_complete_callback_);
+    conn->set_close_callback([this] (const TcpConnectionPtr& p_conn) {
+        this->remove_connection(p_conn);
+    });
+}
+
+void TcpServer::remove_connection(const TcpConnectionPtr &conn)
+{
+    loop_->run_in_loop([this, conn] () {
+        this->remove_connection_in_loop(conn);
+    });
+}
+
+void TcpServer::remove_connection_in_loop(const TcpConnectionPtr &conn)
+{
+    loop_->assert_in_loop_thread();
+    size_t n = connections_.erase(conn->name());
+    assert(n == 1);
+    loop_->queue_in_loop([=] () {
+        conn->connect_destroyed();
+    });
+}
+
+} // namespace icarus
---
+++ icarus/tcpserver.hpp
@@ -0,0 +1,55 @@
+#ifndef ICARUS_TCPSERVER_HPP
+#define ICARUS_TCPSERVER_HPP
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+#include "noncopyable.hpp"
+#include "inetaddress.hpp"
+#include "callbacks.hpp"
+
+namespace icarus
+{
+
+class InetAddress;
+class EventLoop;
+class Acceptor;
+class EventLoopThreadPool;
+
+class TcpServer : noncopyable
+{
+  public:
+    TcpServer(EventLoop* loop, const InetAddress& listen_addr, const std::string& name);
+    ~TcpServer();
+
+    void set_thread_num(int num_threads);
+    void start();
+
+    void set_connection_callback(ConnectionCallback cb);
+    void set_message_callback(MessageCallback cb);
+
+
+  private:
+    void new_connection(int sockfd, const InetAddress& peer_addr);
+    void remove_connection(const TcpConnectionPtr& conn);
+    void remove_connection_in_loop(const TcpConnectionPtr& conn);
+
+    using ConnectionMap = std::unordered_map<std::string, TcpConnectionPtr>;
+
+    EventLoop* loop_;
+    const std::string host_port_;
+    const std::string name_;
+    std::unique_ptr<Acceptor> acceptor_;
+    std::unique_ptr<EventLoopThreadPool> thread_pool_;
+    ConnectionCallback connection_callback_;
+    MessageCallback  message_callback_;
+    WriteCompleteCallback write_complete_callback_;
+    bool started_;
+    int next_conn_id_;
+    ConnectionMap connections_;
+};
+
+} // namespace icarus
+
+#endif //ICARUS_TCPSERVER_HPP
